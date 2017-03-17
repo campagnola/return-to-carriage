713e749e727e5a765ac13fa53f4142f992114c03
Luke Campagnola <luke.campagnola@gmail.com>
2017-03-17 02:08:39 -0700

    vispy working nicely

--- vispylarn.py
+++ vispylarn.py
@@ -37,47 +37,56 @@ class SpritesVisual(vispy.visuals.Visual):
         uniform sampler2D atlas;
         uniform vec2 atlas_size;
         uniform sampler1D atlas_map;
+        uniform float n_sprites;
+        uniform vec2 scale;
         
         void main()
         {
-            vec4 atlas_coords = texture1D(atlas_map, v_sprite);
-            vec2 uv = gl_PointCoord.xy; // 0-1 over point area
-            vec2 tex_coords = atlas_coords.xy + uv * atlas_coords.zw;
-            vec4 tex = texture2D(atlas, tex_coords / atlas_size);
-            gl_FragColor = tex.r * v_fgcolor + tex.g * v_bgcolor;
+            vec4 atlas_coords = texture1D(atlas_map, v_sprite / n_sprites);
+            vec2 pt = gl_PointCoord.xy / scale;
+            if( pt.x < 0 || pt.y < 0 || pt.x > 1 || pt.y > 1 ) {
+                discard;
+            }
+            vec2 tex_coords = atlas_coords.yx + pt * atlas_coords.wz;
+            vec4 tex = texture2D(atlas, tex_coords);
+            gl_FragColor = tex.g * v_fgcolor + tex.b * v_bgcolor;
             gl_FragColor.a = 1.0;
-            gl_FragColor.r = 1.0;
+            gl_FragColor.rgb += 0.1;
         }
     """
 
-    def __init__(self):
+    def __init__(self, shape=(40, 120)):
         vispy.visuals.Visual.__init__(self, self.vertex_shader, self.fragment_shader)
         
-        self.pos = np.random.normal(size=(10, 2), loc=300, scale=100).astype('float32')
-        self.sprites = np.random.randint(0, 5, size=10).astype('float32')
+        self.size = 16
+        scale = (0.6, 1)
+        npts = shape[0]*shape[1]
+        self.pos = np.mgrid[0:shape[1], 0:shape[0]].transpose(1, 2, 0).reshape(npts,2) * (self.size * np.array(scale).reshape(1, 1, 2))
+        self.pos = self.pos.astype('float32')
+        self.sprites = np.random.randint(0, 5, size=shape).astype('float32').reshape(npts)
         
-        self.atlas = np.random.randint(0, 256, size=(10, 50, 3)).astype('ubyte')
-        self.atlas_map = np.zeros((5, 4), dtype='float32')
-        self.atlas_map[:,0] = np.arange(0, 50, 10)
-        self.atlas_map[:,2:] = 10
+        self.atlas = SpriteAtlas()
         
-        self.size = 40
-        self.fgcolor = np.random.normal(size=(10, 4)).astype('float32')
-        self.bgcolor = np.random.normal(size=(10, 4)).astype('float32')
+        self.fgcolor = np.random.normal(size=(npts, 4)).astype('float32')
+        self.bgcolor = np.random.normal(size=(npts, 4)).astype('float32')
 
         self.shared_program['position'] = self.pos
         self.shared_program['sprite'] = self.sprites
         self.shared_program['fgcolor'] = self.fgcolor
         self.shared_program['bgcolor'] = self.bgcolor
         self.shared_program['size'] = self.size
+        self.shared_program['scale'] = scale
         
-        self._atlas_tex = vispy.gloo.Texture2D(data=self.atlas, interpolation='nearest')
+        self._atlas_tex = vispy.gloo.Texture2D(data=self.atlas.atlas, interpolation='nearest')
         self.shared_program['atlas'] = self._atlas_tex
 
         self.shared_program['atlas_size'] = self._atlas_tex.shape[:2]
 
-        self._atlas_map_tex = vispy.gloo.Texture1D(data=self.atlas_map, interpolation='nearest')
+        self._atlas_map_tex = vispy.gloo.Texture1D(data=self.atlas.sprite_coords, interpolation='nearest')
         self.shared_program['atlas_map'] = self._atlas_map_tex
+
+        self.shared_program['n_sprites'] = self._atlas_map_tex.shape[0]
+
 
         self._draw_mode = 'points'
         
@@ -94,13 +103,47 @@ class SpritesVisual(vispy.visuals.Visual):
         
 Sprites = vispy.scene.visuals.create_visual_node(SpritesVisual)
 
+import pyqtgraph as pg
+class SpriteAtlas(object):
+    def __init__(self):
+        chars = "12345"
+        self.size = 32
+        self.font = QtGui.QFont('monospace', self.size)
+        fm = QtGui.QFontMetrics(self.font)
+        char_shape = (int(fm.height()), int(fm.maxWidth()))
+        self.glyphs = np.empty((len(chars),) + char_shape + (3,), dtype='ubyte')
+        for i,char in enumerate(chars):
+            img = QtGui.QImage(char_shape[1], char_shape[0], QtGui.QImage.Format_RGB32)
+            p = QtGui.QPainter()
+            p.begin(img)
+            brush = QtGui.QBrush(QtGui.QColor(255, 0, 0))
+            p.fillRect(0, 0, char_shape[1], char_shape[0], brush)
+            pen = QtGui.QPen(QtGui.QColor(0, 255, 0))
+            p.setPen(pen)
+            p.setFont(self.font)
+            p.drawText(0, fm.ascent(), char)
+            p.end()
+            self.glyphs[i] = pg.imageToArray(img)[..., :3].transpose(1, 0, 2)
+            
+        gs = self.glyphs.shape
+        self.atlas = self.glyphs.reshape((gs[0]*gs[1], gs[2], gs[3]))
+        self.sprite_coords = np.empty((gs[0], 4), dtype='float32')
+        self.sprite_coords[:,0] = np.arange(0, gs[0]*gs[1], gs[1])
+        self.sprite_coords[:,1] = 0
+        self.sprite_coords[:,2] = char_shape[0]
+        self.sprite_coords[:,3] = char_shape[1]
+        
+        self.sprite_coords[:,::2] /= self.atlas.shape[0]
+        self.sprite_coords[:,1::2] /= self.atlas.shape[1]
+
 
 if __name__ == '__main__':
     canvas = vispy.scene.SceneCanvas()
     canvas.show()
+    canvas.size = 1400,1000
     
-    #view = canvas.central_widget.add_view()
-    #view.camera = 'panzoom'
-    #view.camera.rect = [0, 0, 200, 200]
+    view = canvas.central_widget.add_view()
+    view.camera = 'panzoom'
+    view.camera.rect = [0, 0, 200, 200]
     
-    txt = Sprites(parent=canvas.scene)
+    txt = Sprites(parent=view.scene)
