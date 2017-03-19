f17364af3301193540d244d7d602f430ca1a1b5f
Luke Campagnola <luke.campagnola@gmail.com>
2017-03-18 22:06:46 -0700

    refactor

--- vispylarn.py
+++ vispylarn.py
@@ -65,49 +65,67 @@ class SpritesVisual(vispy.visuals.Visual):
         }
     """
 
-    def __init__(self, shape=(40, 120)):
-        self.size = 0
-        self.pos = None
-        self.sprites = None
-        self.atlas = None
-        self.fgcolor = None
-        self.bgcolor = None
-        self._atlas_tex = None
-        self._atlas_map_tex = None
+    def __init__(self, atlas, size=16, scale=1):
+        self.size = size
+        self.scale = scale
+        self.atlas = atlas
+        self.data = np.empty(1, dtype=[('pos', 'float32', 2), 
+                                       ('sprite', 'uint32'), 
+                                       ('fgcolor', 'float32', 4),
+                                       ('bgcolor', 'float32', 4)])
+        
+        self._atlas_tex = vispy.gloo.Texture2D(shape=(1,1,4), format='rgba', interpolation='nearest')
+        self._atlas_map_tex = vispy.gloo.Texture1D(shape=(1,4), format='rgba', interpolation='nearest')
+        self._need_data_upload = False
+        self._need_atlas_upload = False
         
         vispy.visuals.Visual.__init__(self, self.vertex_shader, self.fragment_shader)
         self._draw_mode = 'points'
-       
-    def set_data(self, pos, sprites, atlas, fgcolor, bgcolor, size, scale):
-        self.size = size
-        self.pos = pos.astype('float32').reshape(np.product(pos.shape[:-1]), pos.shape[-1])
-        self.sprites = sprites.reshape(np.product(sprites.shape)).astype('float32')
-        
-        self.atlas = atlas
-        
-        self.fgcolor = fgcolor
-        self.bgcolor = bgcolor
+    
+    def add_sprites(self, n):
+        """Expand to allow n more sprites, return a SpriteData instance.
+        """
+        i = self._resize(self.data.shape[0] + n)
+        return SpriteData(self, i, i+n)
 
-        self.shared_program['position'] = self.pos
-        self.shared_program['sprite'] = self.sprites
-        self.shared_program['fgcolor'] = self.fgcolor
-        self.shared_program['bgcolor'] = self.bgcolor
+    def _resize(self, n):
+        """Resize sprite array, return old size.
+        """
+        n1 = len(self.data)
+        newdata = np.empty(n, dtype=self.data.dtype)
+        newdata[:n1] = self.data[:n]
+        self.data = newdata
+        self._need_data_upload = True
+        return n1
+    
+    def _upload_data(self):
+        self.shared_program['position'] = self.data['pos']
+        self.shared_program['sprite'] = self.data['sprite']
+        self.shared_program['fgcolor'] = self.data['fgcolor']
+        self.shared_program['bgcolor'] = self.data['bgcolor']
         self.shared_program['size'] = self.size
-        self.shared_program['scale'] = scale
-        
-        self._atlas_tex = vispy.gloo.Texture2D(data=self.atlas.atlas, interpolation='nearest')
+        self.shared_program['scale'] = self.scale
+        self._need_data_upload = False
+
+    def _upload_atlas(self):
+        self._atlas_tex.set_data(self.atlas.atlas)
         self.shared_program['atlas'] = self._atlas_tex
-
-        self._atlas_map_tex = vispy.gloo.Texture1D(data=self.atlas.sprite_coords, interpolation='nearest')
+        self._atlas_map_tex.set_data(self.atlas.sprite_coords)
         self.shared_program['atlas_map'] = self._atlas_map_tex
-
         self.shared_program['n_sprites'] = self._atlas_map_tex.shape[0]
-       
+        self._need_atlas_upload = False
+    
     def _prepare_transforms(self, view):
         xform = view.transforms.get_transform()
         view.view_program.vert['transform'] = xform
         
     def _prepare_draw(self, view):
+        if self._need_data_upload:
+            self._upload_data()
+            
+        if self._need_atlas_upload:
+            self._upload_atlas()
+        
         # set point size to match zoom
         tr = view.transforms.get_transform('visual', 'canvas')
         o = tr.map((0, 0))
@@ -121,15 +139,47 @@ class SpritesVisual(vispy.visuals.Visual):
         
 Sprites = vispy.scene.visuals.create_visual_node(SpritesVisual)
 
+
+class SpriteData(object):
+    def __init__(self, sprites, start, stop):
+        self.sprites = sprites
+        self.indices = (start, stop)
+        
+    @property
+    def data(self):
+        start, stop = self.indices
+        return self.sprites.data[start:stop]
+
+
+
 import pyqtgraph as pg
-class SpriteAtlas(object):
-    def __init__(self, chars, size=128):
+
+
+class CharAtlas(object):
+    """Texture atlas containing rendered text characters.    
+    """
+    def __init__(self, size=128):
         self.size = size
         self.font = QtGui.QFont('monospace', self.size)
-        fm = QtGui.QFontMetrics(self.font)
-        char_shape = (int(fm.height()), int(fm.maxWidth()))
-        self.glyphs = np.empty((len(chars),) + char_shape + (3,), dtype='ubyte')
+        self.chars = {}
+        self._fm = QtGui.QFontMetrics(self.font)
+        char_shape = (int(self._fm.height()), int(self._fm.maxWidth()))
+        self.glyphs = np.empty((0,) + char_shape + (3,), dtype='ubyte')
+        self._rebuild_atlas()
+
+    def add_chars(self, chars):
+        """Add new characters to the atlas, return the first new index.
+        """
+        oldn = self.glyphs.shape[0]
+        newglyphs = np.empty((oldn + len(chars),) + self.glyphs.shape[1:], dtype=self.glyphs.dtype)
+        newglyphs[:oldn] = self.glyphs
+        self.glyphs = newglyphs
+        
+        char_shape = self.glyphs.shape[1:3]
+        
         for i,char in enumerate(chars):
+            self.chars[char] = oldn + i
+            
             img = QtGui.QImage(char_shape[1], char_shape[0], QtGui.QImage.Format_RGB32)
             p = QtGui.QPainter()
             p.begin(img)
@@ -138,20 +188,24 @@ class SpriteAtlas(object):
             pen = QtGui.QPen(QtGui.QColor(0, 255, 0))
             p.setPen(pen)
             p.setFont(self.font)
-            p.drawText(0, fm.ascent(), char)
+            p.drawText(0, self._fm.ascent(), char)
             p.end()
-            self.glyphs[i] = pg.imageToArray(img)[..., :3].transpose(1, 0, 2)
-            
+            self.glyphs[oldn+i] = pg.imageToArray(img)[..., :3].transpose(1, 0, 2)
+        
+        self._rebuild_atlas()
+        return oldn
+
+    def _rebuild_atlas(self):
         gs = self.glyphs.shape
         self.atlas = self.glyphs.reshape((gs[0]*gs[1], gs[2], gs[3]))
         self.sprite_coords = np.empty((gs[0], 4), dtype='float32')
         self.sprite_coords[:,0] = np.arange(0, gs[0]*gs[1], gs[1])
         self.sprite_coords[:,1] = 0
-        self.sprite_coords[:,2] = char_shape[0]
-        self.sprite_coords[:,3] = char_shape[1]
-        
+        self.sprite_coords[:,2] = gs[1]
+        self.sprite_coords[:,3] = gs[2]
         self.sprite_coords[:,::2] /= self.atlas.shape[0]
         self.sprite_coords[:,1::2] /= self.atlas.shape[1]
+
 
 
 if __name__ == '__main__':
@@ -164,35 +218,48 @@ if __name__ == '__main__':
     view.camera.rect = [0, 0, 200, 200]
     view.camera.aspect = 1
     
-    txt = Sprites(parent=view.scene)
-
-
-    shape = (50, 120)
-    npts = shape[0]*shape[1]
+    # generate a texture for each character we need
+    atlas = CharAtlas()
+    atlas.add_chars(".#")
+    
+    # create sprites visual
     size = 16
     scale = (0.6, 1)
-        
-    maze = np.ones(shape, dtype=int)
-    maze[1:10, 1:10] = 0
-    maze[-10:-1, -10:-1] = 0
-    maze[20:39, 1:80] = 0
-    maze[5:30, 6] = 0
-    maze[35, 5:-5] = 0
+    txt = Sprites(atlas, size, scale, parent=view.scene)
+
+    # create maze
+    shape = (50, 120)
+    npts = shape[0]*shape[1]
+    maze = txt.add_sprites(npts)
+    mazedata = maze.data.reshape(shape)
     
-    pos = np.mgrid[0:shape[1], 0:shape[0]].transpose(2, 1, 0).reshape(npts,2) * (size * np.array(scale).reshape(1, 1, 2))
-    colors = np.array([
+    # set wall/floor
+    sprites = mazedata['sprite']
+    sprites[:] = 1
+    sprites[1:10, 1:10] = 0
+    sprites[-10:-1, -10:-1] = 0
+    sprites[20:39, 1:80] = 0
+    sprites[5:30, 6] = 0
+    sprites[35, 5:-5] = 0
+
+    # set positions
+    pos = np.mgrid[0:shape[1], 0:shape[0]].transpose(2, 1, 0) * (size * np.array(scale).reshape(1, 1, 2))
+    mazedata['pos'] = pos.astype('float32')
+
+    # set colors
+    sprite_colors = np.array([
         [[0.2, 0.2, 0.2, 1.0], [0.0, 0.0, 0.0, 1.0]],  # path
         [[0.0, 0.0, 0.0, 1.0], [0.2, 0.2, 0.2, 1.0]],  # wall
     ], dtype='float32')
-    color = colors[maze]
-    fgcolor = color[...,0,:]
-    bgcolor = color[...,1,:]
+    color = sprite_colors[sprites]
+    mazedata['fgcolor'] = color[...,0,:]
+    mazedata['bgcolor'] = color[...,1,:]
     
     # randomize wall color a bit
     rock = np.random.normal(scale=0.01, size=shape + (1,))
-    walls = maze == 1
+    walls = sprites == 1
     n_walls = walls.sum()
-    bgcolor[...,:3][walls] += rock[walls]
+    mazedata['bgcolor'][...,:3][walls] += rock[walls]
 
-    atlas = SpriteAtlas('.#')
-    txt.set_data(pos, maze, atlas, fgcolor, bgcolor, size, scale)
+    txt._need_data_upload = True
+    txt._need_atlas_upload = True
