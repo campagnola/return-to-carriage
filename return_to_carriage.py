ce969e9bf7c475732896b039424105d508491588
Luke Campagnola <luke.campagnola@gmail.com>
2021-07-04 21:19:44 -0700

    more torches

--- return_to_carriage.py
+++ return_to_carriage.py
@@ -20,6 +20,22 @@ if __name__ == '__main__':
     scene = Scene(canvas)
 
     scroll = Scroll(location=(scene, (5, 5)), scene=scene)
-    torch = Torch(location=(scene, (17, 12)), scene=scene)
+    torches = [
+        Torch(location=(scene, (17, 8)), scene=scene),
+        Torch(location=(scene, (3, 8)), scene=scene),
+        Torch(location=(scene, (9,  30)), scene=scene),
+        Torch(location=(scene, (32, 41)), scene=scene),
+        Torch(location=(scene, (32, 45)), scene=scene),
+        Torch(location=(scene, (43, 39)), scene=scene),
+
+        Torch(location=(scene, (16, 75)), scene=scene),
+        Torch(location=(scene, (28, 75)), scene=scene),
+        Torch(location=(scene, (40, 75)), scene=scene),
+        Torch(location=(scene, (52, 75)), scene=scene),
+        Torch(location=(scene, (16, 82)), scene=scene),
+        Torch(location=(scene, (28, 82)), scene=scene),
+        Torch(location=(scene, (40, 82)), scene=scene),
+        Torch(location=(scene, (52, 82)), scene=scene),
+    ]
     yeti = Monster(position=(8, 40), scene=scene)
     
